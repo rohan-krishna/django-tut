b95f909298404e545e730216063ee520886b62d3
Rohan Krishna <Rohan.Krishna@csscorp.com>
2017-04-03 20:13:22 +0530

    Code adjusted to reflect model attributes

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 def index(request):
-	return render(request, 'blog/post_list.html',{})
+	posts = Post.objects.all()
+	return render(request, 'blog/post_list.html',{ 'posts' : posts })
